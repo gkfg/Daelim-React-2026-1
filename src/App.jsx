e781c968685b62558544874872088079f316c026
202230336 <202230336@email.daelim.ac.kr>
2026-03-25 10:23:56 +0900

    my-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Profile from './Profile.jsx'
 
-function Profile(){
-  return(
-    <>
-      <img src={reactLogo} alt="reactLogo"/>
-    </>
-  )
-}
-function Profile2(){
-  return(
-    <>
-      <img src={viteLogo} alt="viteLogo"/>
-    </>
-  )
-}
-function Profile3(){
-  return(
-    <>
-      <img src={heroImg} alt="heroImg"/>
-    </>
-  )
-}
 export default function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Profile/>
-      <Profile2/>
-      <Profile3/>
     </>
   )
 }
