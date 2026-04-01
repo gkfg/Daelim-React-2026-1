96bc5fd2aaab7b9754fd64c45f3caf3c31af6a33
202230336 <202230336@email.daelim.ac.kr>
2026-04-01 12:46:37 +0900

    20260401Spread

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import Profile from './components/Profile.jsx'
 import Gallery from './components/Gallery.jsx'
 import UseJsx from './components/UseJsx.jsx'
 import ParentComp from './components/ParentComp.jsx'
-
+import SpreadComp from './components/SpreadComp.jsx'
 export default function App() {
 
   return (
     <>
       {/* <Gallery/> */}
-      <UseJsx/>
-      <ParentComp alt="React" width={100} height={100}/>
+      {/* <UseJsx/>
+      <ParentComp alt="React" width={100} height={100}/> */}
+      <SpreadComp/>
     </>
   )
 }
--- src/components/ChildComp.jsx
+++ src/components/ChildComp.jsx
@@ -1,9 +1,9 @@
 import reactRogo from '../assets/react.svg'
 
-export default function ChildComp([imageInfo, width, height]){
+export default function ChildComp({imageInfo, width, height}){
     return(
         <>
-            <img src={imageInfo.src} alt={alt} width={width} height={height} />
+            <img src={imageInfo.src} alt={imageInfo.alt} width={width} height={height} />
         </>
     )
 }
